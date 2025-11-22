c0d48b9d28fd3c277f305a53ef6048c2bfdcbfe0
arpan7sarkar <arpansakar@gmail.com>
2025-11-22 13:57:41 +0530

    implemented navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
-
+import Navbar  from "./components/Navbar"
 const App = () => {
   return (
-    <div>
-      <div>Hello</div>
-    </div>
+    <main>
+      <Navbar/>
+    </main>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import dayjs from "dayjs";
+
+import { navLinks } from "#constants";
+import { navIcons } from "../constants";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav>
+      <div>
+        <img src="./images/logo.svg" alt="logo" />
+        <p className="font-bold">Arpan's Portfolio</p>
+        <ul>
+          {navLinks.map(({ id, name }) => (
+            <li key={id}>{name}</li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <ul>
+          {navIcons.map(({ id, img }) => (
+            <li key={id}>
+              <img className="icon-hover" src={img} alt="icon-${id}" />
+            </li>
+          ))}
+        </ul>
+        <time datetime="">{dayjs().format("ddd MMM D h:mm A")}</time>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
