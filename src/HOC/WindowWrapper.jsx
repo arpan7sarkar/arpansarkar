2332c4dfba65772b8add38cd9e268885e0f09f89
arpan7sarkar <arpansakar@gmail.com>
2025-11-24 00:40:08 +0530

    Implement Terminal window component to display tech stack, introduce WindowWrapper HOC, and add global styling.

--- src/HOC/WindowWrapper.jsx
+++ src/HOC/WindowWrapper.jsx
@@ -1,13 +1,31 @@
-import { useRef } from "react";
-import  useWindowStore  from "../store/windows";
+import { useLayoutEffect, useRef } from "react";
+import useWindowStore from "../store/windows";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 const WindowWrapper = (Component, windowKey) => {
   const Wrapped = (props) => {
-    const { focusWindow,  windows } = useWindowStore();
+    const { focusWindow, windows } = useWindowStore();
     const { isOpen, zIndex } = windows[windowKey] || {
       isOpen: false,
       zIndex: 0,
     };
     const ref = useRef(null);
+    // if (!isOpen) return null;
+
+    useGSAP(() => {
+      const el = ref.current;
+      if (!el || !isOpen) return;
+      el.style.display = "block";
+    }, [isOpen]);
+
+    useLayoutEffect(() => {
+      const el = ref.current;
+      if (!el) return;
+      el.style.display = isOpen ? "block" : "none";
+
+      gsap.fromTo(el, { opacity: 0, scale: 0.8,y:40 }, { opacity: 1, scale: 1,y:0,duration:0.4,ease:"power3.out" });
+
+    }, [isOpen]);
 
     return (
       <section id={windowKey} ref={ref} style={{ zIndex }} className="absolute">
--- src/windows/Terminal.jsx
+++ src/windows/Terminal.jsx
@@ -1,4 +1,6 @@
-import WindowWrapper from "../HOC/windowWrapper";
+import { Check, Flag } from "lucide-react";
+import { techStack } from "../constants";
+import WindowWrapper from "../HOC/WindowWrapper";
 
 const Terminal = () => {
   return (
@@ -11,8 +13,34 @@ const Terminal = () => {
       <div className="techstack">
         <p>
           <span className="font-bold">@arpan % </span>
-           show tech stack
+          show tech stack
         </p>
+        <div className="label">
+          <p className="w-32 ">Category</p>
+          <p>Technologies</p>
+        </div>
+        <ul className="content">
+          {techStack.map(({ category, items }) => (
+            <li key={category} className="flex items-center">
+              <Check className="check" size={20} />
+              <h3>{category}</h3>
+              <ul>
+                {items.map((item, i) => (
+                  <li key={i}>{item}{i<items.length-1 && ","}</li>
+                ))}
+              </ul>
+            </li>
+          ))}
+        </ul>
+        <div className="footnote">
+            <p>
+                <Check size={20}/>  5 of 5 stacks loaded succesfully (100%)
+            </p>
+            <p className="text-black">
+                <Flag size={15} fill="black"/>
+                Render time 5ms
+            </p>
+        </div>
       </div>
     </div>
   );
