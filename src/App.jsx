c331759ee4aeea185176f00af8b4136ed3e38d12
arpan7sarkar <arpansakar@gmail.com>
2025-11-26 00:38:22 +0530

    added text file opening option

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,28 @@
 import gsap from "gsap";
-import { Dock,Navbar ,Welcome} from "#components"
+import { Dock, Navbar, Welcome } from "#components";
 import { Draggable } from "gsap/Draggable";
-import { TerminalWindow,SafariWindow , ResumeWindow ,FinderWrapper} from "./windows";
-
-
+import {
+  TerminalWindow,
+  SafariWindow,
+  ResumeWindow,
+  FinderWrapper,
+  TextWindow,
+} from "./windows";
 
 gsap.registerPlugin(Draggable);
 const App = () => {
   return (
     <main>
-      <Navbar/>
-      <Welcome/>
-      <Dock/>
-      <TerminalWindow/>
-      <SafariWindow/>
-      <ResumeWindow/>
-      <FinderWrapper/>
+      <Navbar />
+      <Welcome />
+      <Dock />
+      <TerminalWindow />
+      <SafariWindow />
+      <ResumeWindow />
+      <FinderWrapper />
+      <TextWindow />
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/windows/index.js
+++ src/windows/index.js
@@ -2,5 +2,12 @@ import TerminalWindow from "./Terminal";
 import SafariWindow from "./Safari";
 import ResumeWindow from "./Resume";
 import FinderWrapper from "./Finder";
+import TextWindow from "./Text";
 
-export { TerminalWindow, SafariWindow ,ResumeWindow, FinderWrapper};
+export {
+  TerminalWindow,
+  SafariWindow,
+  ResumeWindow,
+  FinderWrapper,
+  TextWindow,
+};
